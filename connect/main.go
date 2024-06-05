5a6895db0c6677fb68e29eebb2d6f83fd1bd3f63
Bashorun97 <bashorun.emma@gmail.com>
2024-06-05 13:07:24 +0000

    Connect golib
    Signed-off-by: Bashorun97 <bashorun.emma@gmail.com>

---
+++ connect/main.go
@@ -0,0 +1,225 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"encoding/base64"
+	"fmt"
+	"log"
+	"strings"
+	"net/url"
+	"github.com/skip2/go-qrcode"
+
+	graphqlClient "github.com/gandalf-network/gandalf-sdk-go/eyeofsauron/graphql"
+)
+
+const APP_CLIP_BASE_URL = "https://appclip.apple.com/id?p=network.gandalf.connect.Clip"
+const SAURON_BASE_URL = "https://sauron.gandalf.network/public/gql"
+
+const (
+	InvalidService GandalfErrorCode = iota
+	InvalidPublicKey
+	InvalidRedirectURL
+	QRCodeGenNotSupported
+	QRCodeNotGenerated
+)
+
+func (e *GandalfError) Error() string {
+	return fmt.Sprintf("%s (code: %d)", e.Message, e.Code)
+}
+
+func GenerateURL(publicKey string, redirectURL string, input Services) (string, error) {
+	services, err := runValidation(publicKey, redirectURL, input)
+	if err != nil {
+		return "", err
+	}
+
+	servicesJSON := servicesToJSON(services)
+
+	return encodeComponents(servicesJSON, publicKey, redirectURL), nil
+}
+
+func GenerateQRCode(publicKey string, redirectURL string, input Services) (string, error) {
+	if publicKey == "" || redirectURL == "" || input == nil {
+		return "", &GandalfError{
+			Message: "Invalid input parameters",
+			Code:    QRCodeGenNotSupported,
+		}
+	}
+
+	services, err := runValidation(publicKey, redirectURL, input)
+	if err != nil {
+		return "", err
+	}
+
+	servicesJSON := servicesToJSON(services)
+	appClipURL := encodeComponents(servicesJSON, redirectURL, publicKey)
+
+	qrCode, err := qrcode.New(appClipURL, qrcode.Medium)
+	if err != nil {
+		return "", &GandalfError{
+			Message: "QRCode Generation Error",
+			Code:    QRCodeNotGenerated,
+		}
+	}
+
+	qrCodeData, err := qrCode.PNG(256)
+	if err != nil {
+		return "", &GandalfError{
+			Message: "QRCode Generation Error",
+			Code:    QRCodeNotGenerated,
+		}
+	}
+	qrCodeURL := fmt.Sprintf("data:image/png;base64,%s", base64.StdEncoding.EncodeToString(qrCodeData))
+	return qrCodeURL, nil
+}
+
+func introspectSauron() IntrospectionResult {
+	client := graphqlClient.NewClient(SAURON_BASE_URL)
+	req := graphqlClient.NewRequest(introspectionQuery)
+
+	ctx := context.Background()
+
+	var respData IntrospectionResult
+
+	if err := client.Run(ctx, req, &respData); err != nil {
+		log.Fatalf("Error making introspection query: %v", err)
+	}
+	return respData
+}
+
+func validateRedirectURL(rawURL string) error {
+	_, err := url.ParseRequestURI(rawURL)
+	if err != nil {
+		return &GandalfError{
+			Message: "Invalid redirect URL",
+			Code:    InvalidRedirectURL,
+		}
+	}
+	return nil
+}
+
+func validatePublicKey(publicKey string) bool {
+	return publicKeyRequest(publicKey)
+}
+
+func getSupportedServices() interface{} {
+	gqlSchema := introspectSauron()
+	for _, val := range gqlSchema.Schema.Types {
+		if val.Kind == "ENUM" && val.Name == "Source" {
+			return val.EnumValues
+		}
+	}
+	return nil
+}
+
+func validateInputServices(input Services) (map[string]Service, error) {
+	supportedServicesInterface := getSupportedServices()
+	supportedServices := supportedServicesInterface.(SupportedServices)
+	
+	serviceMap := make(map[string]SupportedService)
+	for _, val := range supportedServices {
+		serviceMap[val.Name] = val
+	}
+
+	var unsupportedServices []string
+	requiredServices := 0
+	cleanedServices :=  make(map[string]Service)
+	for _, val := range input {
+		key := strings.ToUpper(val.Name)
+		if _, found := serviceMap[key]; !found {
+			unsupportedServices = append(unsupportedServices, key)
+			continue
+		}
+		
+		if val.Name != "" {
+			requiredServices++
+		}
+		cleanedServices[key] = val
+	}
+
+	if len(unsupportedServices) > 0 {
+		return nil, &GandalfError{
+			Message: fmt.Sprintf("These services %s are unsupported", strings.Join(unsupportedServices, " ")),
+			Code:    InvalidService,
+		}
+	}
+
+	if requiredServices < 1 {
+		return nil, &GandalfError{
+			Message: "At least one service has to be required",
+			Code:    InvalidService,
+		}
+	}
+
+	return cleanedServices, nil
+
+}
+
+func publicKeyRequest(publicKey string) bool {
+	graphqlRequest := graphqlClient.NewRequest(`
+	query GetAppByPublicKey($publicKey: String!) {
+	  getAppByPublicKey(publicKey: $publicKey) {
+		  appName
+		  gandalfID
+	  }
+	}
+  `)
+  
+  	graphqlRequest.Var("publicKey", publicKey)
+
+  	client := graphqlClient.NewClient(SAURON_BASE_URL)
+
+	ctx := context.Background()
+
+	var respData Application
+
+	if err := client.Run(ctx, graphqlRequest, &respData); err != nil {
+		log.Fatalf("Error making introspection query: %v", err)
+	}
+
+	return respData.GandalfID > 0
+}
+
+func runValidation(publicKey string, redirectURL string, input Services) (map[string]Service, error) {
+	isPublicKeyValid := validatePublicKey(publicKey)
+	if !isPublicKeyValid {
+		return nil, &GandalfError{
+			Message: "Public key does not exist",
+			Code:    InvalidPublicKey,
+		}
+	}
+
+	err := validateRedirectURL(redirectURL)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return nil, err
+	}
+
+	services, err := validateInputServices(input)
+	if err != nil {
+		return nil, err
+	}
+	return services, nil
+}
+
+func encodeComponents(servicesJSON []byte, publicKey string, redirectURL string) string {
+	encodedServices := url.QueryEscape(string(servicesJSON))
+	encodedRedirectURL := url.QueryEscape(redirectURL)
+	encodedPublicKey := url.QueryEscape(publicKey)
+
+	return fmt.Sprintf("%s&services=%s&redirectUrl=%s&publicKey=%s", APP_CLIP_BASE_URL, encodedServices, encodedRedirectURL, encodedPublicKey)
+}
+
+func servicesToJSON(services map[string]Service) []byte {
+	var servicesSlice []Service
+	for _, service := range services {
+		servicesSlice = append(servicesSlice, service)
+	}
+
+	servicesJSON, err := json.Marshal(servicesSlice)
+	if err != nil {
+		log.Fatalf("Error marshaling JSON: %v", err)
+	}
+	return servicesJSON
+}
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/gandalf-network/genqlient v1.0.2
 	github.com/google/uuid v1.6.0
 	github.com/pkg/errors v0.9.1
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 )
 
 require (
