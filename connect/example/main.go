470d032e609720c5733c0bfef34185f41f681c02
Bashorun97 <bashorun.emma@gmail.com>
2024-06-07 11:54:54 +0000

    Update example code
    Signed-off-by: Bashorun97 <bashorun.emma@gmail.com>

--- connect/example/main.go
+++ connect/example/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"os"
 	"fmt"
 	"log"
 
+	"encoding/base64"
 	"github.com/gandalf-network/gandalf-sdk-go/connect"
+	"strings"
 )
 
 const publicKey = "0x036518f1c7a10fc77f835becc0aca9916c54505f771c82d87dd5943bb01ba5ca08";
@@ -18,8 +21,14 @@ func main() {
 			Activities: []string{"watch"},
 		},
 	}
+
+	config := connect.Config{
+		PublicKey:   publicKey,
+		RedirectURL: redirectURL,
+		Data:    	 services,
+	}
 	
-	conn, err := connect.NewConnect(publicKey, redirectURL, connect.PlatformTypeIOS, services)
+	conn, err := connect.NewConnect(config)
 	if err != nil {
 		log.Fatalf("An error occurred with initializing connect: %v", err)
 	}
@@ -32,11 +41,27 @@ func main() {
 	fmt.Println("URL => ", url)
 	
 	
-	qrCodeURL, err := conn.GenerateQRCode(services)
+	qrCode, err := conn.GenerateQRCode()
 	if err != nil {
 		log.Fatalf("An error occurred generating QR Code url: %v", err)
 	}
-	fmt.Println("QR Code URL => ", qrCodeURL)
+	fmt.Println("Base64 QR Code => ", qrCode)
 
+	var base64QRCodeWithPrefix string
+	if strings.HasPrefix(qrCode, "data:image/png;base64,") {
+		base64QRCodeWithPrefix = strings.TrimPrefix(qrCode, "data:image/png;base64,")
+	}
+	
+	decodedQRCode, err := base64.StdEncoding.DecodeString(base64QRCodeWithPrefix)
+	if err != nil {
+		log.Fatalf("Failed to decode base64 QR code: %v", err)
+	}
 
+	outputFile := "decoded_qrcode.png"
+	err = os.WriteFile(outputFile, decodedQRCode, 0644)
+	if err != nil {
+		log.Fatalf("Failed to save decoded QR code as PNG: %v", err)
+	}
+
+	fmt.Printf("Decoded QR code saved to %s\n", outputFile)
 }
